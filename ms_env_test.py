3ffe0f825df7b9972a9c1dc6cb8d7816685d715f
sjYoondeltar <dbsdns@naver.com>
2024-08-03 15:51:03 +0900

    env action limit set

--- ms_env_test.py
+++ ms_env_test.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 import mani_skill2.envs
 import cv2
+import numpy as np
 
 env = gym.make("PickCube-v0", obs_mode="rgbd", control_mode="pd_joint_delta_pos", render_mode="cameras")
 print("Observation space", env.observation_space)
@@ -8,8 +9,12 @@ print("Action space", env.action_space)
 
 obs, _ = env.reset()
 done = False
-while not done:
-    action = env.action_space.sample()
+for i in range(200):
+    # action = env.action_space.sample()
+    if i < 100:
+        action = np.array([-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0])
+    else:
+        action = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
     obs, reward, terminated, truncated, info = env.step(action)
     render = env.render()  # a display is required to render
     # print("render", render)
