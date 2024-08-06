f02bd71c0aa32dabc142c313bb73eedf0653f29f
sjYoondeltar <dbsdns@naver.com>
2024-08-07 00:47:39 +0900

    buffer storage test

--- ms_env_test.py
+++ ms_env_test.py
@@ -15,7 +15,7 @@ import gymnasium as gym
 import mani_skill2.envs
 import utils
 from logger import Logger
-from replay_buffer import ReplayBufferStorage, make_replay_loader
+from replay_buffer_ms2 import ReplayBufferStorage, make_replay_loader
 from video import TrainVideoRecorder, VideoRecorder
 
 torch.backends.cudnn.benchmark = True
@@ -46,6 +46,34 @@ def main(cfg):
     print("Action space", env.action_space)
     
     rgb_obs_shape = (2, 3, 84, 84)
+    
+    work_dir = Path.cwd()
+    
+    # create replay buffer
+    data_specs = (
+        specs.Array((2, 3, 84, 84), np.uint8, "rgb_obs"),
+        specs.Array((9,), np.float32, "qpos"),
+        specs.Array((8,), np.float32, "action"),
+        specs.Array((1,), np.float32, "reward"),
+        specs.Array((1,), np.float32, "discount"),
+        specs.Array((1,), np.float32, "demo"),
+    )
+    
+    replay_storage = ReplayBufferStorage(
+        data_specs, work_dir / "buffer", True
+    )
+    
+    replay_loader = make_replay_loader(
+        work_dir / "buffer",
+        cfg.replay_buffer_size,
+        cfg.batch_size,
+        cfg.replay_buffer_num_workers,
+        cfg.save_snapshot,
+        cfg.nstep,
+        cfg.discount,
+        cfg.do_always_bootstrap,
+        cfg.frame_stack,
+    )
 
     agent = make_ms2_agent(
                 rgb_obs_shape,
@@ -57,6 +85,7 @@ def main(cfg):
 
     obs, _ = env.reset()
     terminated = False
+    truncated = False
     global_step = 0
     for i in range(200):
         # action = env.action_space.sample()
@@ -70,6 +99,19 @@ def main(cfg):
         action = agent.act(rgb_obs, low_dim_obs, global_step, True)
         
         obs, reward, terminated, truncated, info = env.step(action)
+        
+        inst_samples = {
+            'rgb_obs': rgb_obs.numpy().astype(np.uint8),
+            'qpos': low_dim_obs.numpy(),
+            'action': action,
+            'reward': reward,
+            'discount': 0.99,
+            'demo': 0.0,
+            'last': terminated or truncated,
+        }
+        
+        replay_storage.add(inst_samples)
+        
         # render = env.render()  # a display is required to render
         print("reward", reward)
         # print("render", render)
